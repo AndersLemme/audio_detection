5e563941612a6ab0455e9a7b2adc295d5f3e3d05
AndersLemme <a.lemme@hotmail.com>
2024-12-18 13:14:44 +0100

    Now i rewrote the script, It doesnt save wav file anymore, but it gets the audo in a fomrmat that can be passed to librosa.mfcc

--- model/runCNN2.py
+++ model/runCNN2.py
@@ -7,6 +7,7 @@ import struct
 import librosa #load files and convert to MFCC's
 import librosa.display
 import matplotlib.pyplot as plt #plot mfccs
+from prepare_aduio import crop_audio
 
 
 import pyaudio #realtime capture
@@ -40,54 +41,53 @@ class Recorder:
         self.stream_params = stream_params
         self._pyaudio = None
         self._stream = None
-        #self._wav_file = None
 
         
     def record(self, duration: int, save_path: str):
         """ Record audio from mic for a given amount of time"""
 
-        self._create_recording_resources(save_path)
-        self._write_wav_file_reading_from_stream(save_path, duration)
-        self._close_recording_resources()
+        self.create_recording_resources(save_path)
+        audio_signal = self.reading_from_stream(save_path, duration) #record audio
+        self.close_recording_resources()
         print("Stop recording")
+        return audio_signal
         
-    def _create_recording_resources(self, save_path: str) -> None:
+    def create_recording_resources(self, save_path: str) -> None:
         self._pyaudio= pyaudio.PyAudio()
         self._stream = self._pyaudio.open(**self.stream_params.to_dict())
         
         
-    def _create_wav_file(self, save_path: str):
-        self._wav_file = wave.open(save_path, "wb")
-        self._wav_file.setnchannels(self.stream_params.channels)
-        self._wav_file.setsampwidth(self._pyaudio.get_sample_size(self.stream_params.format))
-        self._wav_file.setframerate(self.stream_params.rate)
 
-
-
-    def _write_wav_file_reading_from_stream(self, save_path, duration: int) -> None:
-        
+    def reading_from_stream(self, save_path, duration: int) -> None:
+        """Capture audio data and convert it to a NumPy array."""
+        print("start recording...") 
         audio_data_frames = []
         
-        x=np.arange(0,2*self.stream_params.frames_per_buffer,2)
+        #x=np.arange(0,2*self.stream_params.frames_per_buffer,2)
         
         for _ in range(int(stream_params.rate * duration / self.stream_params.frames_per_buffer)):
             audio_data = self._stream.read(self.stream_params.frames_per_buffer)
-            audio_data_frames.append(audio_data) #append audio data to array
-            dataInt = struct.unpack(str(self.stream_params.frames_per_buffer) + 'h', audio_data) #not neccessary at all!
+            #audio_data_frames.append(audio_data) #append audio data to array
+            audio_data_frames.extend(struct.unpack(str(self.stream_params.frames_per_buffer) + 'h', audio_data))
+
+            #dataInt = struct.unpack(str(self.stream_params.frames_per_buffer) + 'h', audio_data) #not neccessary at all!
             
             #if (check_amplitude(dataInt, 15000)): #(any(i > 15000 for i in dataInt)): #(dataInt[_] > 10000):
             #    print("High amplitude!! -> send signal to PLC about bag pop")
             #    pop += 1
         #if (pop > 0): #if pop write to file, if not don't write
         #predict_output(self, save_path , audio_data_frames)
+        print("Recording complete")
+        audio_signal = np.array(audio_data_frames, dtype=np.float32) / 32768.0 #normalize
+        return audio_signal
 
-
-    def _close_recording_resources(self) -> None:
-        try:
-            self._wav_file.close()
-            print(f"pop occured, wav file saved to {OUTPUT_PATH}pop")
-        except Exception as e:
-            print("no pop has occured, Wav file will not be saved")
+    def close_recording_resources(self) -> None:
+        #try:
+        #    self._wav_file.close()
+        #    print(f"pop occured, wav file saved to {OUTPUT_PATH}pop")
+        #except Exception as e:
+        #    print("no pop has occured, Wav file will not be saved")
+        self._stream.stop_stream()
         self._stream.close()
         self._pyaudio.terminate()
         
@@ -105,16 +105,81 @@ def predict_output(self, save_path, audio_data) -> None:
 
 
 if __name__ == "__main__":
-   
+    #Define counter for predicted pop/nopop
+    cnt_pop = 0
+    cnt_nopop = 0
+
     stream_params = StreamParams()
     recorder = Recorder(stream_params)
+    n_mfcc=13
+    n_fft=2048
+    hop_length=512
+    num_segments=1
+    sr = 48000
+    SAMPLES_PER_FILE = StreamParams.rate*DURATION
     
     for i in range(2):
         file_path = os.path.join(OUTPUT_PATH, "audio_{}.wav".format(datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S")))
         audio = recorder.record(DURATION, file_path)
         print("audio == : ", audio)
 
+        duration = librosa.get_duration(y=audio, sr=sr)
+        print(f"Duration: {duration:.2f} seconds")
 
+        if(duration != DURATION):
+            audio = crop_audio(DURATION, sr ,audio)
+
+
+        n_sample_seg = int(SAMPLES_PER_FILE/num_segments)   
+        expected_number_mfcc_per_segment = np.ceil(n_sample_seg/hop_length)#
+
+        ii=0
+
+        for s in range(num_segments): #for each segments -- (kept at 1)
+            start_sample = n_sample_seg *s
+            finish_sample = start_sample + n_sample_seg
+
+            print("audio shape: ",audio.shape)
+
+            mfcc = librosa.feature.mfcc(y = audio[start_sample:finish_sample], 
+                                                sr=sr,
+                                                n_mfcc=n_mfcc,
+                                                n_fft=n_fft,
+                                                hop_length=hop_length)
+            mfcc = mfcc.T
+            print("MFCC shape",mfcc.shape)
+
+
+            X= mfcc[np.newaxis, ...]
+            predict = MODEL.predict([X])
+            
+            print(predict)
+            print(int(np.round(predict)))
+            print(CATEGORIES[int((np.round(predict)))])
+
+            y_pred_round = int(np.round(predict))
+            y_pred_category = CATEGORIES[int((np.round(predict)))]
+
+            # Visualize MFCC coefficients
+            #plt.figure()
+            #librosa.display.specshow(mfcc, x_axis='time')
+            #plt.colorbar()
+            #plt.title('MFCC{}| file({}) | prediction = {} = {} '.format(ii,file, predict, y_pred_category))
+            #plt.tight_layout()
+            #plt.show()
+
+            #plt.savefig('mfcc{}_file-{}_prediction-{}_category-{}.png'.format(ii,file, predict, y_pred_category))
+
+
+            #return prediction 1 or 9
+            prediction = int(np.round(predict))
+            if(predict == 0):
+                cnt_nopop += 1
+            elif(predict==1):
+                cnt_pop += 1
+            
+        #Print number predictions
+        print("Predicted nopops = {} | Predicted pops = {}".format(cnt_nopop, cnt_pop))
 
 
 
